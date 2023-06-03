ac5216eee2efbb738c9011865ebd668255234a12
DangPhamQuocBao <quocbao0165@gmail.com>
2023-06-03 22:29:38 +0700

    lan3

--- controller/helper.js
+++ controller/helper.js
@@ -1,4 +1,3 @@
-// hàm giúp gán giá trị mới cho input
 function ganGiaTriChoInput(
   taiKhoan,
   hoTen,
@@ -19,11 +18,9 @@ function ganGiaTriChoInput(
   document.getElementById("gioLam").value = gioLam;
 }
 
-// hàm tìm vị trí sinh viên dựa vào mã sinh viên
 function timViTriNhanVien(taiKhoan) {
   var viTri = -1;
   arrNhanVien.forEach(function (item, index) {
-    // item ở đây là một sinhVien nằm trong array arrSinhVien nên mới chấm tới thuộc tính maSinhVien
     if (item.taiKhoan == taiKhoan) {
       viTri = index;
     }
@@ -32,16 +29,12 @@ function timViTriNhanVien(taiKhoan) {
 }
 
 function layGiaTriInput() {
-  // lấy dữ liệu người dùng
-
   var _taiKhoan = document.getElementById("tknv").value;
-  //   console.log(_maSinhVien.length);
   var _hoTen = document.getElementById("name").value;
   var _email = document.getElementById("email").value;
   var _matKhau = document.getElementById("password").value;
   var _ngayLam = document.getElementById("datepicker").value;
   var _luongCoBan = document.getElementById("luongCB").value;
-  //   console.log(_khoaHoc);
   var _chucVu = document.getElementById("chucvu").value * 1;
   var _gioLam = document.getElementById("gioLam").value * 1;
 
@@ -58,12 +51,10 @@ function layGiaTriInput() {
 
   valid &= kiemTraEmail(_email, "tbEmail");
 
-  // ở đây chúng ta kiểm tra biến valid, nếu valid là false thì sẽ return không chạy những đoạn lệnh bên dưới
   if (!valid) {
     return;
   }
 
-  // khi lấy được dữ liệu, gọi tới lớp đội tượng sinh viên và tạo ra đối  tượng sinh viên
   var nhanVien = new NhanVien(
     _taiKhoan,
     _hoTen,
@@ -77,17 +68,13 @@ function layGiaTriInput() {
   return nhanVien;
 }
 
-// Hàm lưu dữ liệu xuống local
 function saveStorage(arrNhanVien) {
   localStorage.setItem("arrNhanVien", JSON.stringify(arrNhanVien));
 }
 
-// Hàm lấy giá trị từ local lên và sử dụng
 function getStorage() {
-  // check điều kiện nếu như key gọi tới không có dưới local
   var arrNhanVienLocal = JSON.parse(localStorage.getItem("arrNhanVien"));
   if (arrNhanVienLocal != null) {
-    // nếu như arrSinhVienLocal có giá trị sẽ lấy giá trị đó gán mới vào cho mảng arrSinhVien đang có
     arrNhanVien = arrNhanVienLocal;
   }
 }
--- controller/index.js
+++ controller/index.js
@@ -67,17 +67,14 @@ renderGiaoDien();
 
 function renderGiaoDien() {
   var content = "";
-  // khi chúng ta gọi dữ liệu từ localStorage lên và sử dụng, các object bên trong mảng arrSinhVien đã bị mất đi các phương thức
-  // idea là sẽ tạo ra một đối tượng mới từ lớp đối tượng SinhVien đang có và gán cho đối tượng đó tất cả thuộc tính đang có của từng obejct bên trong arrSinhVien sau khi gọi getStorage
 
   for (var i = 0; i < arrNhanVien.length; i++) {
     var nhanVien = new NhanVien();
-    // console.log(sinhVien);
+
     var nhanVienItem = arrNhanVien[i];
-    // console.log(sinhVienItem);
-    // Object.assign giúp clone ra một object mới với các thuộc tính
+
     Object.assign(nhanVien, nhanVienItem);
-    // var sinhVien = arrSinhVien[i];
+
     var tinhTien = nhanVien.tinhTongLuong();
     var xepLoai = nhanVien.xepLoai();
     content += `
@@ -101,44 +98,21 @@ function renderGiaoDien() {
   }
   document.getElementById("tbodySinhVien").innerHTML = content;
 }
-// renderGiaoDien();
 
 function themNhanVien() {
-  // check nếu điểm toán bé hơn 0 và lớn hơn 10 sẽ cho một thông báo là nhập sai và sau đó dừng hàm
-  // if (_diemToan < 0 || _diemToan > 10) {
-  //   alert("Nhập sai rồi dongok");
-  //   return;
-  // }
-  // var valid = kiemTraRong();
-
   var nhanVien = layGiaTriInput();
-  // console.log(sinhVien);
-  // sẽ check nếu như sinhVien bị undifined sẽ chặn hết các hành động bên dưới
+
   if (nhanVien) {
     arrNhanVien.push(nhanVien);
     saveStorage(arrNhanVien);
-    // render lên giao diện bằng mảng vừa thêm vô
+
     renderGiaoDien();
 
-    // reset input khi người dùng thêm sinh viên thành công
     ganGiaTriChoInput("", "", "", "", "", "", "", "");
   }
 }
 
 document.getElementById("btnThemNV").onclick = themNhanVien;
-
-// Yêu cầu: tạo ra một phương thức ở lớp đối tượng dùng để xếp loại sinh viên,
-// nếu sinh viên trên 8 sẽ là giỏi, trên 5 dưới 8 là khá, còn dưới 5 là kém
-
-/** Xoá sinh viên khỏi mảng
- * đầu tiên một function chạy chức năng xoá
- * bên trong function chạy một vòng lặp để duyệt mảng
- * bên trong vòng lặp sẽ check điều kiện, điều kiện là check maSinhVien của sinh viên ngay tại lúc bấm trùng với bất kì một maSinhVien nào trong mảng thì sẽ trả về index của sinhVien ngay tại vị trí đó trong mảng, còn nếu không thì trả về là -1
- * dùng hàm từ javascript để xoá sinhVien, sẽ dùng hàm splice nhận vào 2 giá trị, giá trị đầu là vị trí index, giá trị t2 là số phần tử cần xoá
- * sau khi xoá, phải chạy lại hàm renderGiaoDien một lần nữa để upload lại dữ liệu mới lên giao diện
- */
-
-// CRUD (create,read,delete,update)
 
 function xoaNhanVien(taiKhoan) {
   var index = timViTriSinhVien(taiKhoan);
@@ -148,13 +122,6 @@ function xoaNhanVien(taiKhoan) {
     renderGiaoDien();
   }
 }
-
-/** Edit sinhVien (Sửa sinh viên)
- * tìm vị trí của sinhVien đó trong mảng
- * cho nút cập nhật thông tin sinh viên hiển thị khi bấm edit
- * lấy phần tử tìm được trong mảng hiển thị lên cho người dùng chỉnh sửa thông qua input
- *
- */
 
 function editNhanVien(taiKhoan) {
   document.getElementById("btnCapNhat").style.display = "inline-block";
@@ -175,10 +142,9 @@ function editNhanVien(taiKhoan) {
 
 function capNhatThongTinNhanVien() {
   var nhanVienDaChinhSua = layGiaTriInput();
-  // console.log(sinhVienDaChinhSua);
+
   var index = timViTriNhanVien(nhanVienDaChinhSua.taiKhoan);
-  // console.log(index);
-  // sau khi tìm được vị trí index của phần đang chỉnh sửa trong mảng, chúng ta sẽ làm một việc là thay thế phần tử đó trong mảng bằng giá trị mới
+
   arrNhanVien[index] = nhanVienDaChinhSua;
   saveStorage(arrNhanVien);
   renderGiaoDien();
